b30f9af4f14a795f993196729daf74a22156c794
adityapatel-sudo <adityapatel1204@gmail.com>
2020-07-12 08:54:39 -0700

    Doing the Find cycle node problem using Hashset

--- src/com/company/LinkedList/LinkedListCycleDetector.java
+++ src/com/company/LinkedList/LinkedListCycleDetector.java
@@ -1,8 +1,22 @@
 package com.company.LinkedList;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class LinkedListCycleDetector {
-    public static ListNode detectCycle(){
-        return null;
+    public static ListNode detectCycle(MyLinkedList.Node head){
+        var nodesSet = new HashSet<MyLinkedList.Node>();
     }
 }
