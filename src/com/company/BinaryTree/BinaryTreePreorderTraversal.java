93d244bdea478de8bbe1c445935234f4d646770c
adityapatel-sudo <adityapatel1204@gmail.com>
2020-09-28 12:48:19 -0700

    Did Linkedlist and started Binary Tree Problems- Sept 2020

--- src/com/company/BinaryTree/BinaryTreePreorderTraversal.java
+++ src/com/company/BinaryTree/BinaryTreePreorderTraversal.java
@@ -1,25 +1,22 @@
 package com.company.BinaryTree;
 
-import com.sun.source.tree.Tree;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.*;
 
 class Solution {
     public List < Integer > inorderTraversal(TreeNode root) {
         List < Integer > res = new ArrayList < > ();
-        helper(root, res);
+        goUntilNull(root, res);
         return res;
     }
 
-    public void helper(TreeNode root, List < Integer > res) {
+    public void goUntilNull(TreeNode root, List < Integer > res) {
         if (root != null) {
             if (root.left != null) {
-                helper(root.left, res);
+                goUntilNull(root.left, res);
             }
             res.add(root.val);
             if (root.right != null) {
-                helper(root.right, res);
+                goUntilNull(root.right, res);
             }
         }
     }
