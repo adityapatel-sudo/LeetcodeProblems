3ac170d6639ebf82870c3f57477fec7725a2290f
adityapatel-sudo <adityapatel1204@gmail.com>
2021-03-23 17:53:31 -0700

    changes

--- src/com/company/SchoolProblems/SortBubble.java
+++ src/com/company/SchoolProblems/SortBubble.java
@@ -12,20 +12,32 @@ public class SortBubble {
         for(int i = 0; i < 10;i++){
             array.add(sc.nextInt());
         }
-        System.out.println(array);
+        System.out.print("bubbleSort\n");
+        bubbleSort();
+
+        System.out.println(array+"\n");
+
     }
     public static void bubbleSort(){
         int ival;
-        for(int i = 0 ; i < array.size()-1;i++){
-            ival = array.get(i);
-            for (int j = i+1; j< array.size();j++){
-
-                if(ival<array.get(j)){
-                    array.add(j,ival);
-                    array.remove(i);
+        int i = 0;
+        for(int cursor = 0 ; cursor < array.size()-1;cursor++,i++){
+            if(array.get(i)>array.get(i+1)){
+                ival = array.get(i);
+                for (int j = i; j< array.size();j++){
+                    if(ival<array.get(j)){
+                        array.add(j,ival);
+                        array.remove(i);
+                        break;
+                    }else if (j == array.size()-1){
+                        array.add(ival);
+                        array.remove(i);
+                        break;
+                    }
                 }
+                i--;
             }
+            System.out.println(array);
         }
     }
-
 }
--- src/com/company/SchoolProblems/WaysToClimb.java
+++ src/com/company/SchoolProblems/WaysToClimb.java
@@ -2,15 +2,12 @@ package com.company.SchoolProblems;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class WaysToClimb {
     static int numberWays = 0;
-    static ArrayList<ArrayList<Integer>> order;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of stairs to climb (positive integer): ");
         int len = sc.nextInt();
-        order = new ArrayList<>();
         recu(new ArrayList<Integer>(), len);
         System.out.println("Total number of ways = " + numberWays);
     }
